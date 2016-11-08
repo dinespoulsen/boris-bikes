9b22774b1530bbb1f0668025489bcf2d99f4b8f6
TudorTacal <tudortacal@gmail.com>
2016-11-08 16:42:13 +0000

    Completed challenge 10, created a bike object and checked if it's working

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -2,11 +2,13 @@ require "docking_station"
 
 describe DockingStation do
 
-  subject { docking_station = DockingStation.new }
+  describe "should respond to" do
+    subject { docking_station = DockingStation.new }
+    it { is_expected.to respond_to(:release_bike)}
+  end
 
-  it { is_expected.to respond_to(:release_bike)}
-
-  
-
-
+  describe "it gets a bike and checks if it's working" do
+    subject { bike = DockingStation.new.release_bike;  bike.working?}
+    it {is_expected.to be true}
+  end
 end
