0a5e33530156314b601f8c719a81a6749e5f2b92
Chris Cooper <cjcooper252@gmail.com>
2016-11-10 15:56:32 +0000

    reports bike as broken

--- spec/bike_spec.rb
+++ spec/bike_spec.rb
@@ -1,5 +1,19 @@
 require "bike"
 
 describe Bike do
-  it { is_expected.to respond_to :working? }
+  it { is_expected.to respond_to :report_broken }
+
+  it { is_expected.to respond_to :broken}
+
+  it 'is working at the start' do
+    bike = Bike.new
+    expect(bike.broken).to eq false
+  end
+
+  it 'reports that a bike is broken' do
+    bike = Bike.new
+    bike.report_broken
+    expect(bike.broken).to eq true
+  end
+
 end
