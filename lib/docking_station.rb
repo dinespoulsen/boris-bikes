4d7c245cf2dd3ae96cfdd657c110944be0e75e8f
pflarcology <rhrg500@gmail.com>
2016-11-10 12:37:49 +0000

    added private area, refactored code

--- lib/docking_station.rb
+++ lib/docking_station.rb
@@ -9,13 +9,24 @@ attr_reader :bikes
   end
 
   def release_bike
-    fail "No bikes available" if @bikes.empty?
+    fail "No bikes available" if empty?
     @bikes.pop
   end
 
   def dock(bike)
-    fail "dock is full" if @bikes.count >=20
+    fail "dock is full" if full?
     @bikes << bike
   end
+
+private
+
+  def full?
+    @bikes.count >=20
+  end
+
+  def empty?
+    bikes.empty?
+  end
+
 
 end
